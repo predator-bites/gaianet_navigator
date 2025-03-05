609604f86a6c93b1d80fbb1ce50ed2ac32730fe9
DropPredator <142865899+predator-bites@users.noreply.github.com>
2025-03-05 22:20:02 +0200

    Update gaianet_navigator.sh

--- gaianet_navigator.sh
+++ gaianet_navigator.sh
@@ -9,7 +9,7 @@ RESET='\033[0m'
 logo() {
     echo -e "
 ${RESET}  ____   ${NEON_BLUE}____
-${RESET} |  _ \\   ${NEON_BLUE}|  _ \\
+${RESET} |  _ \\  ${NEON_BLUE}|  _ \\
 ${RESET} | | | | ${NEON_BLUE}| |_) |
 ${RESET} | |_| | ${NEON_BLUE}|  __/
 ${RESET} |____/  ${NEON_BLUE}|_|
